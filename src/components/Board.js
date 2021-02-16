d9b20021942011edb29902f58b75375d5bef29c8
Drew Bradley <williamandrewb@gmail.com>
2021-02-16 09:34:34 -0700

    Add Board.js and Card.js and styling.

---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Board.css'
+import Card from './Card'
+
+const Board = () => {
+  return (
+    <section className="game-board"></section>
+  )
+}
+
+export default Board
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css'
+
+const Card = () => {
+  return (
+    <article className="card">
+      <div className="card-front"></div>
+      <div className="card-back"></div>
+    </article>
+  )
+}
+
+export default Card
